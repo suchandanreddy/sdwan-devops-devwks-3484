c1b54de682841648d251d60aa0f4075425290dd8
suchandan <saisuchandan@gmail.com>
2020-01-26 17:04:59 +0100

    cleanup changes

--- cleanup.sh
+++ cleanup.sh
@@ -4,10 +4,10 @@ docker stop grafana-influxdb_container
 
 docker rm grafana-influxdb_container
 
-source venv/bin/activate
+sudo source venv/bin/activate
 
 jupyter notebook stop
 
-python3 cleanup-webhooks.py
+sudo python3 cleanup-webhooks.py
 
 /opt/cisco/anyconnect/bin/vpn disconnect dcloud-rtp-anyconnect.cisco.com
